b43119f37d13fd9a2d3b7b87c801f1f5b0cd4910
Dan4ef <bdanilaf3@gmail.com>
2023-02-06 12:29:33 +0200

    Add project files.

---
+++ WebApplication1/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Models
+{
+    public class Book
+    {
+        /// <summary>
+        /// ID книги
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// назва книги
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// автор книги
+        /// </summary>
+        public string Author { get; set; }
+        /// <summary>
+        /// цiна
+        /// </summary>
+        public int Price { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Purchase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WebApplication1.Models
+{
+    public class Purchase
+    {
+        /// <summary>
+        /// ID покупки
+        /// </summary>
+        public int PurchaseId { get; set; }
+        /// <summary>
+        /// ім'я й прізвище покупця
+        /// </summary>
+        public string Person { get; set; }
+        /// <summary>
+        /// адреса покупця
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// ID книги
+        /// </summary>
+        public int BookId { get; set; }
+        /// <summary>
+        /// дата покупки
+        /// </summary>
+        public DateTime Date { get; set; }
+    }
+}
